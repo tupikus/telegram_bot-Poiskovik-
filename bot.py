c3dcafbf1f0ff550c483cb6c77f7c50e9e0901de
tupikus <pureexplosion@gmail.com>
2016-11-19 11:45:15 +0400

    Рабочий бот

--- bot.py
+++ bot.py
@@ -1,9 +1,20 @@
-import telebot
 import config
+import telebot
+import sqlite3
 
 # Настраиваем соединение с нашим ботом
-#Turn
-#Turn2
+bot = telebot.TeleBot(config.token)
+
+#Бот отвечает на команды
+@bot.message_handler(commands = ['start','help'])
+def send_welcome(message):
+    bot.reply_to(message, "Ёу, чел")
+#Бот отвечает на все сообщения по умолчанию
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+#Запускаем бота
+bot.polling()
 
 
 
